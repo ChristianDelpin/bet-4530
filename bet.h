38bfae15edec9631e905aa7095a156f3cd423a7a
firefin <christianakd@live.com>
2024-11-08 21:05:42 -0500

    defined all things, but make does not work.

--- bet.h
+++ bet.h
@@ -1,28 +1,23 @@
+#ifndef BET_H
+#define BET_H
+
 class BET
 {
     public:
         BET();
-        BET(const string postfix);
+        BET(const std::string postfix);
         BET(const BET& b);
         ~BET();
         
         bool empty();
-        bool buildFromPostfix(const string postfix);
+        bool buildFromPostfix(const std::string postfix);
         void printInfixExpression();
         void printPostfixExpression();
         size_t size();
         size_t leaf_nodes();
         
         const BET& operator=(const BET& b);
-
     private:
-    //ALL REQUIRED PRIVATE MEMBERS MUST BE IMPLEMENTED RECURSIVELY
-        void printInfixExpression(BinaryNode* n);
-        void makeEmpty(BinaryNode*& t);
-        BinaryNode* clone(BinaryNode* t) const;
-        void printPostfixExpression(BinaryNode* n);
-        size_t size(BinaryNode* n);
-        size_t leaf_nodes(BinaryNode* n);
 
         struct BinaryNode
         {
@@ -32,4 +27,16 @@ class BET
             BinaryNode(const std::string& e, BinaryNode* l = nullptr, BinaryNode* r = nullptr)
             : element(e), left(l), right(r) {}
         };
+
+        BinaryNode* root;
+    //ALL REQUIRED PRIVATE MEMBERS MUST BE IMPLEMENTED RECURSIVELY
+        void printInfixExpression(BinaryNode* n);
+        void makeEmpty(BinaryNode*& t);
+        BinaryNode* clone(BinaryNode* t) const;
+        void printPostfixExpression(BinaryNode* n);
+        size_t size(BinaryNode* n);
+        size_t leaf_nodes(BinaryNode* n);
+
+        
 };
+#endif
